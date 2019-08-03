71059ecb57d18c9979ebcb70a1509194c553932b
JordanWilson <mrwizard6933@gmail.com>
2019-08-03 18:42:07 -0400

    correctly positioned the adventure story modal

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -21,7 +21,7 @@ export class DashboardComponent implements OnInit {
     this.adventureName = event;
     setTimeout(() => {
       this.adventureChosen = true;
-    }, 1490); // this is the length of time the animation takes to complete
+    }, 1450); // this is the length of time the animation takes to complete
 
   }
 
